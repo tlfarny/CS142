eafc41cdc71906775392042141da94d299368851
Travis Farnsworth <travis.l.farnsworth@gmail.com>
2016-10-21 14:38:09 -0600

    Add files via upload

---
+++ Project5/main.cpp
@@ -0,0 +1,405 @@
+/*TEST CASES
+ 
+ TEST 1:
+ 
+ For test 1, I just wanted to make sure that all of the functions were transferred properly when I refactored my code. In addition, it made it so I could check that the total and averaging commands that I had put in place were working correctly. So I just used 2 chips when asked, all chips were placed in slot 4 when asked, and then I quit the program. Results are as follows.
+ 
+ TEST 1 RESULTS:
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 1
+ Enter the number of the slot in which you would like to drop the chip:
+ 4
+ The path of your chip was:
+ 4
+ 3.5
+ 3
+ 2.5
+ 3
+ 2.5
+ 2
+ 2.5
+ 3
+ 3.5
+ 3
+ 3.5
+ 4
+ The amount of money that you won is $10000.
+ 
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 2
+ Enter the number of chips that you would like to drop into the Plinko machine:
+ 2
+ Enter the number of the slot in which you would like to drop your chips:
+ 4
+ The total amount of money won was $0.
+ The average amount of money won for each chip was $0.
+ 
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 3
+ Enter the number of chips that you would like to drop into the Plinko machine:
+ 2
+ The total amount of money won for slot 0 was $600.
+ The average amount of money won for each chip in slot 0 was $300.
+ 
+ The total amount of money won for slot 1 was $10100.
+ The average amount of money won for each chip in slot 1 was $5050.
+ 
+ The total amount of money won for slot 2 was $1500.
+ The average amount of money won for each chip in slot 2 was $750.
+ 
+ The total amount of money won for slot 3 was $20000.
+ The average amount of money won for each chip in slot 3 was $10000.
+ 
+ The total amount of money won for slot 4 was $10000.
+ The average amount of money won for each chip in slot 4 was $5000.
+ 
+ The total amount of money won for slot 5 was $1000.
+ The average amount of money won for each chip in slot 5 was $500.
+ 
+ The total amount of money won for slot 6 was $2000.
+ The average amount of money won for each chip in slot 6 was $1000.
+ 
+ The total amount of money won for slot 7 was $2000.
+ The average amount of money won for each chip in slot 7 was $1000.
+ 
+ The total amount of money won for slot 8 was $1000.
+ The average amount of money won for each chip in slot 8 was $500.
+ 
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 4
+ Quitting....
+ 
+ 
+ TEST 2:
+ 
+ For Test 2, I wanted to make sure that all of the break commands/Error messages were working properly. For this, I made errors on the main menu, which slot for options 1 and 2, how many chips for options 2 and 3.
+ 
+ TEST 2 RESULTS:
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ This
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 1
+ Enter the number of the slot in which you would like to drop the chip:
+ 9
+ Error. Returned to Menu.
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 1
+ Enter the number of the slot in which you would like to drop the chip:
+ -1
+ Error. Returned to Menu.
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 2
+ Enter the number of chips that you would like to drop into the Plinko machine:
+ -1
+ Error. Returned to Menu.
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 3
+ Enter the number of chips that you would like to drop into the Plinko machine:
+ 0
+ Error. Returned to Menu.
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 4
+ Quitting....
+
+
+ TEST 3:
+ 
+ For Test 3, I wanted to ensure that the program could handle number intensive inputs. I placed 10,000 chips in the plinko machine, both for option 2 and 3, and then I quit the machine.
+ 
+ TEST 3 RESULTS:
+ 
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 2
+ Enter the number of chips that you would like to drop into the Plinko machine:
+ 10000
+ Enter the number of the slot in which you would like to drop your chips:
+ 4
+ The total amount of money won was $25723800.
+ The average amount of money won for each chip was $2572.
+ 
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 3
+ Enter the number of chips that you would like to drop into the Plinko machine:
+ 10000
+ The total amount of money won for slot 0 was $7701100.
+ The average amount of money won for each chip in slot 0 was $770.
+ 
+ The total amount of money won for slot 1 was $10463200.
+ The average amount of money won for each chip in slot 1 was $1046.
+ 
+ The total amount of money won for slot 2 was $16020400.
+ The average amount of money won for each chip in slot 2 was $1602.
+ 
+ The total amount of money won for slot 3 was $22795400.
+ The average amount of money won for each chip in slot 3 was $2279.
+ 
+ The total amount of money won for slot 4 was $25134500.
+ The average amount of money won for each chip in slot 4 was $2513.
+ 
+ The total amount of money won for slot 5 was $23156600.
+ The average amount of money won for each chip in slot 5 was $2315.
+ 
+ The total amount of money won for slot 6 was $15689900.
+ The average amount of money won for each chip in slot 6 was $1568.
+ 
+ The total amount of money won for slot 7 was $10624600.
+ The average amount of money won for each chip in slot 7 was $1062.
+ 
+ The total amount of money won for slot 8 was $7773400.
+ The average amount of money won for each chip in slot 8 was $777.
+ 
+ Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.
+ 1 - Drop One Chip into One Slot
+ 2 - Drop Multiple Chips in One Slot
+ 3 - Drop One Chip in All Slots
+ 4 - Quit
+ 4
+ Quitting....
+
+ 
+ */
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+string Menu(){
+    string input;
+    cout<<"Please enter if you would like to drop one chip, multiple chips, drop a number of chips in each slot, or quit the program.\n1 - Drop One Chip into One Slot\n2 - Drop Multiple Chips in One Slot\n3 - Drop One Chip in All Slots\n4 - Quit\n";
+    cin>>input;
+    return input;
+}
+
+int SlotOption(bool flag){
+    int Slot;
+    if (flag==true) {
+        cout<<"Enter the number of the slot in which you would like to drop the chip:\n";
+    }
+    else if (flag==false) {
+        cout<<"Enter the number of the slot in which you would like to drop your chips:\n";
+    }
+    cin>>Slot;
+    if (Slot<0||Slot>=9) {
+        cout<<"Error. Returned to Menu.\n";
+        return 999;
+    }
+    return Slot;
+}
+
+int TotalMaker(int FinalSlot, int TotalScore){
+    const int Slot08Score = 100;
+    const int Slot17Score = 500;
+    const int Slot26Score = 1000;
+    const int Slot35Score = 0;
+    const int Slot44Score = 10000;
+    if (FinalSlot==0 || FinalSlot==8) {
+        TotalScore = TotalScore + Slot08Score;
+    }
+    if (FinalSlot==1 || FinalSlot==7) {
+        TotalScore = TotalScore + Slot17Score;
+    }
+    if (FinalSlot==2 || FinalSlot==6) {
+        TotalScore = TotalScore + Slot26Score;
+    }
+    if (FinalSlot==3 || FinalSlot==5) {
+        TotalScore = TotalScore + Slot35Score;
+    }
+    if (FinalSlot==4) {
+        TotalScore = TotalScore + Slot44Score;
+    }
+    return TotalScore;
+}
+
+int ChipProcess(int Slot,int Chips, bool flag){
+    double SlotChange;
+    int TotalScore = 0;
+    int FinalSlot;
+    if (flag==true) {
+        cout<<"The path of your chip was:\n"<<Slot<<endl;
+    }
+    for (int g=1; g<=Chips; g++) {
+        for (int i = 1; i<=12; i++) {
+            int random1 = rand()% 2+1;
+            if (random1 ==1) {
+                if (i==1) {
+                    if (Slot==0) {
+                        SlotChange=Slot+0.5;
+                    }
+                    else{
+                        SlotChange=Slot-0.5;
+                    }
+                }
+                else{
+                    SlotChange=SlotChange-0.5;
+                }
+                if (SlotChange<=0) {
+                    if (i==12) {
+                        SlotChange = SlotChange;
+                    }
+                    else{
+                        if (flag==true) {
+                            cout<<SlotChange<<endl;
+                        }
+                        SlotChange = SlotChange+0.5;
+                        i++;
+                    }
+                }
+            }
+            else if (random1==2){
+                if (i==1) {
+                    if (Slot==8) {
+                        SlotChange=Slot-0.5;
+                    }
+                    else{
+                        SlotChange=Slot+0.5;
+                    }
+                }
+                else{
+                    SlotChange=SlotChange+0.5;
+                }
+                if (SlotChange>=8) {
+                    if (i==12) {
+                        SlotChange = SlotChange;
+                    }
+                    else{
+                        if (flag==true) {
+                            cout<<SlotChange<<endl;
+                        }
+                        SlotChange = SlotChange-0.5;
+                        i++;
+                    }
+                }
+            }
+            if (flag==true) {
+                cout<<SlotChange<<endl;
+            }
+        }
+        FinalSlot = SlotChange;
+        TotalScore = TotalMaker(FinalSlot, TotalScore);
+    }
+    return TotalScore;
+}
+
+int ChipChooser(){
+    cout<<"Enter the number of chips that you would like to drop into the Plinko machine:\n";
+    int Chips;
+    cin>>Chips;
+    if (Chips<=0) {
+        cout<<"Error. Returned to Menu.\n";
+        return 999;
+    }
+    return Chips;
+}
+
+
+
+int main () {
+    double Slot;
+    int TotalScore;
+    int Chips=1;
+    string input = "";
+    bool flag;
+    
+    srand(time(0));
+    while (input!="4"){
+        input = Menu();
+        while (input=="1") {
+            flag = true;
+            Chips = 1;
+            TotalScore = 0;
+            Slot = SlotOption(flag);
+            if (Slot==999) {
+                break;
+            }
+            TotalScore = ChipProcess(Slot, Chips, flag);
+            cout<<"The amount of money that you won is $"<<TotalScore<<".\n\n";
+            break;
+        }
+        
+        while (input=="2") {
+            TotalScore=0;
+            Chips=1;
+            flag=false;
+            Chips = ChipChooser();
+            if (Chips==999) {
+                break;
+            }
+            Slot = SlotOption(flag);
+            if (Slot==999) {
+                break;
+            }
+            TotalScore = ChipProcess(Slot, Chips, flag);
+            cout<<"The total amount of money won was $"<<TotalScore<<".\n";
+            cout<<"The average amount of money won for each chip was $"<<TotalScore / Chips<<".\n\n";
+            break;
+        }
+        
+        while (input=="3") {
+            TotalScore = 0;
+            flag = false;
+            Chips = ChipChooser();
+            if (Chips==999) {
+                break;
+            }
+            for (int Slot=0; Slot<=8; Slot++) {
+                TotalScore = ChipProcess(Slot, Chips, flag);
+                cout<<"The total amount of money won for slot "<<Slot<<" was $"<<TotalScore<<".\n";
+                cout<<"The average amount of money won for each chip in slot "<<Slot<<" was $"<<TotalScore / Chips<<".\n\n";
+            }
+            break;
+        }
+        
+    }
+    if (input=="4"){
+        cout<<"Quitting....\n\n";
+        return 0;
+    }
+}
